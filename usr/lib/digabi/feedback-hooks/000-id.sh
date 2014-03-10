47a8c7e250dcdec6882afa5675087227dd35f433
Ville Korhonen <ville.korhonen@ylioppilastutkinto.fi>
2014-03-10 03:26:05 +0200

    Create boot-id once, and re-use it.

--- usr/lib/digabi/feedback-hooks/000-id.sh
+++ usr/lib/digabi/feedback-hooks/000-id.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-cat /dev/urandom |head -n 10 |md5sum |awk '{print $1}'
+ID_FILE="/var/lib/bootid"
+
+if [ -f "${ID_FILE}" ]
+then
+    BOOT_ID="$(cat ${ID_FILE})"
+else
+    BOOT_ID="$(cat /dev/urandom |head -n 10 |md5sum |awk '{print $1}')"
+    echo ${BOOT_ID} >${ID_FILE}
+fi
+echo "Boot-ID: ${BOOT_ID}"
