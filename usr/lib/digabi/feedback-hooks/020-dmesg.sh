764130c041d0e4b2f0f641c403e2ec4278859c60
Ville Korhonen <ville.korhonen@ylioppilastutkinto.fi>
2014-03-10 04:42:07 +0200

    Collect full dmesg log.

--- usr/lib/digabi/feedback-hooks/020-dmesg.sh
+++ usr/lib/digabi/feedback-hooks/020-dmesg.sh
@@ -5,7 +5,7 @@ COMMAND="/bin/dmesg"
 if [ -x "${COMMAND}" ]
 then
     echo "I: Executing command (${COMMAND})."
-    ${COMMAND} 2>&1 |grep -i error
+    ${COMMAND} 2>&1
 else
     echo "E: Can't execute command (${COMMAND})."
 fi
